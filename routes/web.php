1977437d8f141c5416e54bdb290c26f440f7793a
williamkmp <williamkurnia123@gmail.com>
2023-05-24 23:59:47 +0700

    [ADD] routes: home, doSearh, doLogout

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,37 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware("guest")->group(function () {
 
-Route::get("/", function () {
-    return redirect()->route("home");
-});
+    Route::get("/", function () {
+        return redirect()->route("home");
+    });
 
-Route::controller(AuthenticationController::class)
-    ->prefix("auth")
-    ->group(function () {
+    Route::controller(AuthenticationController::class)->prefix("auth")->group(function () {
         Route::get("login", "showLogin")->name("login");
         Route::get("register", "showRegister")->name("register");
-
         Route::post("login", "doLogin")->name("doLogin");
         Route::post("register", "doRegister")->name("doRegister");
     });
+});
+
+
+Route::middleware("auth")->group(function () {
+
+    Route::middleware("roleIs:Admin,Member")->group(function(){
+        //TODO: implement all routes
+        Route::get("/home", [HomeController::class, "showHome"])->name("home");
+        Route::get("/dump", [HomeController::class, "showDump"])->name("dump");
+        Route::get("/search", [HomeController::class, "doSearch"])->name("doSearch");
+        Route::get("/doLogout", [AuthenticationController::class, "doLogout"])->name("doLogout");
+
+    });
+
+    Route::middleware("roleIs:Admin")->group(function(){
+        //TODO: implement all routes
+    });
+});
