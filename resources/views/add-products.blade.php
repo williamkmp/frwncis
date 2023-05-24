9e3c1366030911a2478498fa8ea137ee04040297
williamkmp <williamkurnia123@gmail.com>
2023-05-25 04:05:20 +0700

    [INIT] blank view

---
+++ resources/views/add-products.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+
+@section('page-head')
+@endsection
+
+
+@section('page-content')
+    <h1>Add Products</h1>
+@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+
+@section('page-head')
+@endsection
+
+
+@section('page-content')
+    <h1>PROFILE</h1>
+@endsection
