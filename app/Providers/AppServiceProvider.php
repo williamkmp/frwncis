300bd0c15fc474cda73f0f49b1245127f7d3edd6
williamkmp <williamkurnia123@gmail.com>
2023-05-25 23:25:25 +0700

    [ADD] "time_greater_than" validation rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('time_greater_than', function ($attribute, $value, $parameters, $validator) {
+            $openingHour = $validator->getData()[$parameters[0]];
+            return strtotime($value) > strtotime($openingHour);
+        });
+
+        Validator::replacer('time_greater_than', function ($message, $attribute, $rule, $parameters) {
+            $start = str_replace("_" , " ", $parameters[0]);
+            $end = str_replace("_" , " ",$attribute);
+            return "the " . $start . " must be greater than the ". $end;
+        });
     }
 }
