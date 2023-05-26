374413e6631eea6c0dabc3e284cd20085f992919
williamkmp <williamkurnia123@gmail.com>
2023-05-26 13:22:08 +0700

    [UPD] implement addItem()

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CartItem;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -18,12 +21,29 @@ class CartController extends Controller
 
     public function addItem($product_id)
     {
-        //TODO: implement this
+        $product_id = intval($product_id);
+        $user = User::find(Auth::user()->id);
+        $cartItem = $user->cartItems()->where("product_id", $product_id)->first();
+        $itemInCart = ($cartItem != null);
+
+        if($itemInCart){
+            //incremnet
+            $cartItem->quantity += 1;
+        }else{
+            $cartItem = new CartItem;
+            $cartItem->user_id = Auth::user()->id;
+            $cartItem->product_id = $product_id;
+            $cartItem->quantity = 1;
+        }
+
+        $cartItem->save();
+        return redirect()->back();
     }
 
     public function deleteItem($product_id)
     {
-        //TODO: implement this
+        $product_id = intval($product_id);
+
     }
 
     public function decrementItem($product_id)
