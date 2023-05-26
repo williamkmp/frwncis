bc197b132d2180fc76e91de796fa76e813894ca4
williamkmp <williamkurnia123@gmail.com>
2023-05-26 12:15:54 +0700

    [ADD] hasMany CartItem relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,4 +21,9 @@ class Product extends Model
         'description',
         'image_path',
     ];
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
