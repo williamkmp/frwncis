6fd1ed69ea614e826c7b14305bb98469e6166ee5
williamkmp <williamkurnia123@gmail.com>
2023-05-26 13:21:47 +0700

    [ADD] add/remove to cart btn for member user

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -35,11 +35,13 @@
                 @foreach ($products as $product)
                     <div class="col flex-grow-0">
                         <div class="card h-100" style="width: 18rem;">
-                            <img src="{{ asset($product->image_path) }}" class="card-img-top h-50" alt="product-image">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $product->name }}</h5>
-                                <p class="card-text">{{ number_format($product->price, 0, '.', ',') }}</p>
-                                <p class="card-text">{{ $product->description }}</p>
+                            <img src="{{ asset($product->image_path) }}" class="card-img-top" alt="product-image">
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title fw-bolder">{{ $product->name }}</h5>
+                                <p class="card-text mb-5">{{ $product->description }}</p>
+                                <div class="flex-grow-1 w-100"></div>
+                                <p class="card-text fw-bolder">Rp. {{ number_format($product->price, 0, '.', ',') }}</p>
+
                                 @if (Auth::user()->role == 'Admin')
                                     <div>
                                         <a href="{{ route('editProduct', ['product_id' => $product->id]) }}"
@@ -47,6 +49,23 @@
                                         <a href="{{ route('doDeleteProduct', ['product_id' => $product->id]) }}"
                                             class="btn btn-danger">Delete</a>
                                     </div>
+                                @endif
+
+                                @if (Auth::user()->role == 'Member')
+                                    <a href="{{ Auth::user()->cartItems()->where('product_id', $product->id)->exists()? route('doCartDelete', ['product_id' => $product->id]): route('doAddCart', ['product_id' => $product->id]) }}"
+                                        class="btn {{ Auth::user()->cartItems()->where('product_id', $product->id)->exists()? 'btn-danger': 'btn-primary' }} w-100 d-flex align-items-center justify-content-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z">
+                                            </path>
+                                        </svg>
+                                        @if (Auth::user()->cartItems()->where('product_id', $product->id)->exists())
+                                            Remove From Cart
+                                        @else
+                                            Add To Product
+                                        @endif
+                                    </a>
                                 @endif
                             </div>
                         </div>
