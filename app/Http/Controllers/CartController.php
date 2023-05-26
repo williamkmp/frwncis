84e2182e722ea370eda0bb844c3f6aaa558aa0ee
williamkmp <williamkurnia123@gmail.com>
2023-05-26 12:21:25 +0700

    [INIT] method stubs

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function showCart()
+    {
+        //TODO: implement this
+    }
+
+    public function doCheckout()
+    {
+        //TODO: implement this
+    }
+
+    public function addItem($product_id)
+    {
+        //TODO: implement this
+    }
+
+    public function deleteItem($product_id)
+    {
+        //TODO: implement this
+    }
+
+    public function decrementItem($product_id)
+    {
+        //TODO: implement this
+    }
 }
