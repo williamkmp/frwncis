8e2867940af90c4a2e27721a545c162da7e64b8e
williamkmp <williamkurnia123@gmail.com>
2023-05-26 22:27:53 +0700

    [ADD] image_path column tu User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image_path')->nullable()->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
