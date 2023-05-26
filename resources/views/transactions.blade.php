74a4a2846a132ae6011055b504e047a8a3540ea8
williamkmp <williamkurnia123@gmail.com>
2023-05-26 23:20:28 +0700

    [FIX] row numbering

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -2,11 +2,11 @@
 
 
 @section('page-head')
-<style>
-    td{
-       align-items: center;
-    }
-</style>
+    <style>
+        td {
+            align-items: center;
+        }
+    </style>
 @endsection
 
 
@@ -26,10 +26,11 @@
                 </tr>
             </thead>
             <tbody>
+                @php($num = 1)
                 @forelse  ($transactions as $header)
                     @foreach ($header->transactionDetails as $item)
                         <tr>
-                            <th scope="row">{{ $loop->iteration + $loop->parent->index }}</th>
+                            <th scope="row">{{ $num }}</th>
                             <td scope="col">{{ $header->user->name }}</td>
                             <td scope="col">{{ $header->location->city }}</td>
                             <td scope="col">{{ $header->created_at }}</td>
@@ -37,12 +38,13 @@
                             <td scope="col">{{ number_format($item->price, 0, '.', ',') }}</td>
                             <td scope="col">{{ $item->quantity }}</td>
                             <td scope="col">{{ number_format($item->subtotal, 0, '.', ',') }}</td>
+                            @php($num++)
                         </tr>
                     @endforeach
                     <tr style="vertical-align: center">
                         <td>&nbsp;</td>
-                        <td colspan="3" >Total Price: Rp {{ number_format($header->total, 0, '.', ',') }}</td>
-                        <td colspan="1" >Pickup Status
+                        <td colspan="3">Total Price: Rp {{ number_format($header->total, 0, '.', ',') }}</td>
+                        <td colspan="1">Pickup Status
                             @if ($header->isPicked)
                                 <span class="text-success">Picked Up</span>
                             @else
@@ -51,14 +53,17 @@
                         </td>
                         <td colspan="3">
                             <span class="fw-bold">Set Pickup Status</span>
-                            <a
-                                class="btn btn-primary btn-sm ms-2"
-                            href="{{ route('doPickup', ['transaction_id' => $header->id]) }}">
+                            <a class="btn btn-primary btn-sm ms-2"
+                                href="{{ route('doPickup', ['transaction_id' => $header->id]) }}">
                                 Picked Up</a>
                         </td>
                     </tr>
                 @empty
-                    <p class="text-center">No Transaction</p>
+                    <tr>
+                        <td colspan="8">
+                            <p class="text-center">No Transaction</p>
+                        </td>
+                    </tr>
                 @endforelse
 
             </tbody>
